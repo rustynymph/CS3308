6ae37c6667537116f7204442ab9696f642b74ffa
shadowfax6894 <anniekelly6894@gmail.com>
2015-02-22 14:49:17 -0700

    added config for security

--- Fireproof.py
+++ Fireproof.py
@@ -11,7 +11,7 @@ def main():
 	masterusername = sys.argv[1]
 	masterpassword = sys.argv[2]
 	
-	account = masterAccount(masterusername,masterpassword,0)
-	return masterAccount.createMasterAccount(account)
+	account = MasterAccount(masterusername,masterpassword,0)
+	return MasterAccount.insertMasterAccount(account)
 
 main()
--- User.py
+++ User.py
@@ -2,22 +2,30 @@
 
 import MySQLdb as mdb
 import sys
+from config import *
 
-class masterAccount:
+class MasterAccount:
 	def __init__(self,username,password,idNum):
 		self.username = username
 		self.password = password
 		self.idNum = idNum
 	
-	def createMasterAccount(self):
-		con = mdb.connect('localhost','testuser','testAnnie','Fireproof');
+	#def encrypt(self):
+	
+	def insertMasterAccount(self):
+		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
 
 		with con:
 			cur = con.cursor()
-			#cur.execute("DROP TABLE IF EXISTS FireproofAccountLogin")
+			cur.execute("DROP TABLE IF EXISTS FireproofAccountLogin")
 			cur.execute("CREATE TABLE FireproofAccountLogin(Id INT PRIMARY KEY AUTO_INCREMENT, \
 					 UserName VARCHAR(100), PasswordName VARCHAR(100))")
 			cur.execute("INSERT INTO FireproofAccountLogin(UserName) VALUES (%s)", self.username)
 			cur.execute("INSERT INTO FireproofAccountLogin(PasswordName) VALUES (%s)", self.password)
+			
+	def retrieveMasterAccount(self):
+		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
 
+		with con:
+			cur = con.cursor()	
 
