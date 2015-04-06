c242931f848339085ddb33ba1f740516940ef449
shadowfax6894 <anniekelly6894@gmail.com>
2015-04-06 15:25:01 -0600

    using random iv now

--- LoginFunctions.py
+++ LoginFunctions.py
@@ -8,14 +8,20 @@ from FireproofProgram import *
 
 class LoginFunctions:
 
+	accounts = []
+
 	@staticmethod
 	def Login(masterusername,masterpassword):
 		if(not(masterusername) or not(masterpassword)):
 			tkMessageBox.showinfo("Error","Please enter username and password.")
 			return False
 		else:
-			account = MasterAccount(masterusername,masterpassword)
-			if(not(MasterAccount.retrieveMasterAccount(account))):
+			loginAccount = None
+			for account in LoginFunctions.accounts:
+				if (masterusername == account.username) and (masterpassword == account.password):
+					loginAccount = account
+			#account = MasterAccount(masterusername,masterpassword)
+			if(not(MasterAccount.retrieveMasterAccount(loginAccount))):
 				tkMessageBox.showinfo("Create Login","Username and password not found. Please click create an account.")
 				return False
 				
@@ -38,5 +44,6 @@ class LoginFunctions:
 			#elif:
 			else:
 				account = MasterAccount(masterusername,masterpassword)
+				LoginFunctions.accounts += [account]
 				MasterAccount.insertMasterAccount(account)
 
--- MasterAccount.py
+++ MasterAccount.py
@@ -17,8 +17,8 @@ class MasterAccount:
 		self.username = username
 		self.password = password
 		key_hash_obj = Encryption.hashPassword(self.password) #sha256 more secure than md5
-		#self.iv = os.urandom(16)
-		self.iv = 'abcdefghijklmnop'
+		self.iv = os.urandom(16)
+		#self.iv = 'abcdefghijklmnop'
 		self.key = key_hash_obj.digest()
 		self.username_enc = Encryption.encryptCredentials(self.key,self.iv,self.username)
 		self.password_enc = Encryption.encryptCredentials(self.key,self.iv,self.password)
@@ -33,13 +33,14 @@ class MasterAccount:
 			cur.execute("SELECT Id FROM FireproofAccountLogin WHERE (UserName,PasswordName) = (%s,%s)", (self.username_enc,self.password_enc))
 			id_number = cur.fetchone()	
 			self.idNum = id_number				
-			
-	def retrieveMasterAccount(self):
+	
+	@staticmethod		
+	def retrieveMasterAccount(account):
 		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
 
 		with con:
 			cur = con.cursor()	
-			cur.execute("SELECT Id FROM FireproofAccountLogin WHERE (UserName,PasswordName) = (%s,%s)", (self.username_enc,self.password_enc))
+			cur.execute("SELECT Id FROM FireproofAccountLogin WHERE (UserName,PasswordName) = (%s,%s)", (account.username_enc,account.password_enc))
 			id_number = cur.fetchone()
 			
 			if not(id_number):
@@ -51,12 +52,12 @@ class MasterAccount:
 				account_pass = cur.fetchone()
 			
 				if(account_name and account_pass):
-					username = Encryption.decryptCredentials(self.key,self.iv,account_name[0])
-					password = Encryption.decryptCredentials(self.key,self.iv,account_pass[0])
+					username = Encryption.decryptCredentials(account.key,account.iv,account_name[0])
+					password = Encryption.decryptCredentials(account.key,account.iv,account_pass[0])
 					print username
 					print password
 					print id_number
-					return MasterAccount(username,password)
+					return account
 				else:
 					return None
 
