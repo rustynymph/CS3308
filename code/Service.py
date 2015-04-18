4a27e4311e8cc8c2526c3d13f752eeb216ea9233
shadowfax6894 <anniekelly6894@gmail.com>
2015-04-18 12:37:37 -0600

    added service funcs/classes

--- code/Service.py
+++ code/Service.py
@@ -62,22 +62,3 @@ class Service:
 			cur = con.cursor()
 			cur.execute("CREATE TABLE FireproofServicesAccounts (id INT(6) PRIMARY KEY AUTO_INCREMENT,serviceid INT(6),\
 				masterid INT(6),ServiceUsername VARCHAR(30) NOT NULL,ServicePassword VARCHAR(30) NOT NULL)")		
-
-class ServiceAccount:
-
-	def __init__(self,username,password):
-		self.username = username
-		self.password = password
-
-		#self.username_enc
-		#self.password_enc
-
-	@staticmethod
-	def insertServiceAccount(account,service):
-		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
-
-		with con:
-			cur = con.cursor()
-			insert_serviceaccount_command = "INSERT INTO FireproofServicesAccounts (serviceid,masterid,username,password) VALUES (%s,%s,%s,%s)"
-			#cur.execute(insert_servicename_command,(self.id_num,service.service_name))
-			cur.execute(insert_serviceaccount_command,(service.id_num,account.id_num,service.username,service.password))
