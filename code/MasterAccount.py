915555e97bf7429411169eb22fa3e88dfe83b96b
shadowfax6894 <anniekelly6894@gmail.com>
2015-04-18 11:54:56 -0600

    added service retrieval funcs

--- code/MasterAccount.py
+++ code/MasterAccount.py
@@ -46,24 +46,28 @@ class MasterAccount:
 
 		return id_number
 
-	def insertServiceName(self,service):
+	@staticmethod
+	def insertServiceName(account,service):
+		print "heyayayayaa"
+
 		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
 
 		with con:
 			cur = con.cursor()
 			insert_servicename_command = "INSERT INTO FireproofServices (masterid,ServiceName) VALUES (%s,%s)"
 			#cur.execute(insert_servicename_command,(self.id_num,service.service_name))
-			cur.execute(insert_servicename_command,(self.id_num,"Facebook"))
+			cur.execute(insert_servicename_command,(account.id_num,"Facebook"))
 
-	def retrieveServiceName(self,service):
-		account_id = self.id_num
+	@staticmethod
+	def retrieveServiceName(account,service):
+		account_id = account.id_num
 
 		con = mdb.connect(MYSQL_LOC,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DBNAME);
 
 		with con:
 			cur = con.cursor()
-			retrieve_service_name_command = "SELECT id FROM FireproofServices WHERE (masterid) = (%s)"
-			cur.execute(retrieve_service_name_command,(account_id))
+			retrieve_service_name_command = "SELECT id FROM FireproofServices WHERE (masterid,ServiceName) = (%s,%s)"
+			cur.execute(retrieve_service_name_command,(account_id,service.service_name))
 			service_primary_key = cur.fetchone()
 			print service_primary_key		
 
